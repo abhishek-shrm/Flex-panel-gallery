58b9ca89ce2ce2ae758a7fc7f53e9800b6ab9f05
ABHISHEK SHARMA <abhishek.shrm.2195@gmail.com>
2018-12-31 16:13:57 +0530

    final files upload

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-//for js
+//For opening and closing of image
+const panels=document.querySelectorAll('.panel');
+function toggleOpen(){
+  this.classList.toggle('open');
+}
+
+//For appearing of text
+function toggleActive(e){
+  if(e.propertyName.includes('flex')){
+    this.classList.toggle('open-active');
+  }
+}
+
+panels.forEach((panel)=>{
+  panel.addEventListener('click',toggleOpen);
+});
+panels.forEach((panel)=>{
+  panel.addEventListener('transitionend',toggleActive);
+});
